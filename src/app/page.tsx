7cc8d9f3a22c2b74e29924b22c0a8871a3ded750
joon <sargadil@gmail.com>
2024-05-20 23:28:11 +0900

    update redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+'use client';
 import { cn } from "@/lib/utils";
 import { Social } from "@/app/_assets/social";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 export default function Home() {
-  console.log(process.env.NEXTAUTH_URL);
+  const { data: session } = useSession();
+  if (!session){
+    redirect("/words");
+  }
   return (
     <div className="centered w-full flex-col gap-6 p-3">
       <div className="centered w-full flex-col gap-y-4">
         <h1 className={cn("text-3xl font-semibold")}>🔐 Log In</h1>
         <p className="text-sm text-muted-foreground">Welcome back!</p>
-        <p>{process.env.NEXTAUTH_URL}</p>
       </div>
       <Social />
     </div>
