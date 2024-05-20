83fc4dca3a32a823ffde8d89bdcad956df603f07
joon <sargadil@gmail.com>
2024-05-21 00:13:23 +0900

    test console

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 export default function Home() {
   const { data: session } = useSession();
+  console.log(session)
   if (session) {
     redirect("/words");
   }
