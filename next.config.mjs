dc3ed386a6cbaa4f8f722bfcdfff69b8e3298d4b
JoonPark <123704085+Joonpark4@users.noreply.github.com>
2024-05-21 14:01:47 +0900

    update next config Permissions-Policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // 모든 페이지에 대해 헤더를 설정하거나 특정 경로에 대해서만 설정할 수 있습니다.
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Permissions-Policy",
+            value: "compute-pressure=(self)", // 여기에서 필요한 정책을 설정합니다.
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
