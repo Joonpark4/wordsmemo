abaf2613cd95432d8a2ed457b23cc2f63b6c420b
joon <sargadil@gmail.com>
2024-05-22 03:52:37 +0900

    GET request type

--- src/app/api/account/route.ts
+++ src/app/api/account/route.ts
@@ -1,6 +1,6 @@
-export async function GET(email: string) {
+export async function GET(req: Request) {
   const res = await fetch(
-    `https://strapi.joondev.com/api/wordsets?filters[email][$eq]=${email}&fields=email`,
+    `https://strapi.joondev.com/api/wordsets?filters[email][$eq]=sargadil@gmail.com&fields=email`,
     {
       headers: {
         Authorization: `Bearer ${process.env.API_TOKEN_FOR_DATA_STRAPI}`,
